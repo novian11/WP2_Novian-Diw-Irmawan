9fae86197ce66c29431ad09c177ab66d499f66b6
novian11 <133918070+novian11@users.noreply.github.com>
2023-09-25 22:41:37 +0700

    menambahkan latihan1.php

---
+++ latihan1.php
@@ -0,0 +1,12 @@
+<?php
+class latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo    "<h1>Perkenalkan </h1>";
+        echo    "<p>Nama Saya Novian Dwi Irmawan</p>
+                <p>Tempat lahir saya di Brebes, 27 November 1996</p>
+                <p>Sekarang tinggal di cikarang karena saya sedang belajar di UBSI Cikarang</p>
+                olahraga yang saya sukai bersepeda mengitari kota suka suka</p>";
+    }
+}
