5d99fac5a7c8840b10ef406261a7b5aa94ad2680
novian11 <133918070+novian11@users.noreply.github.com>
2023-10-07 00:35:35 +0700

    menambahkan view-latihan.php

---
+++ view-latihan.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>latihan 1 </title>
+</head>
+<body>
+    Halo KAwan.. Yuk kita Belajar Web Programing.. !!!<br>
+    Nilai 1 = <? $nilai1; ?>
+    Nilai 2 = <? $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=$nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+</html>
